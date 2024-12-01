0b94ddd656988d72f36ce92cc906d10c4939a637
fonter <serfin009@gmail.com>
2024-12-01 18:44:58 +0100

    added base schedule table

--- src/app/schedules/ScheduleTable.tsx
+++ src/app/schedules/ScheduleTable.tsx
@@ -1,29 +1,186 @@
-'use client'
+"use client"
 
 import { Schedule } from "./Schedule";
-import ScheduleRow from "./ScheduleRow";
+import * as React from "react"
+import {
+  ColumnDef,
+  flexRender,
+  getCoreRowModel,
+  getPaginationRowModel,
+  useReactTable,
+} from "@tanstack/react-table"
+import { MoreHorizontal } from "lucide-react"
+
+import { Button } from "@/components/ui/button"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table"
+
+import { redirect } from "next/navigation"
 
 type ScheduleTableProps = {
     schedules: Schedule[]
 }
 
+export const columns: ColumnDef<Schedule>[] = [
+    {
+        accessorKey: "jobSlug",
+        header: "Job slug",
+        cell: ({ row }) => (
+          <div>{row.getValue("jobSlug")}</div>
+        ),
+    },
+    {
+        accessorKey: "description",
+        header: "Job description",
+        cell: ({ row }) => (
+          <div>{row.getValue("description")}</div>
+        ),
+    },
+    {
+        accessorKey: "frequency",
+        header: "Frequency",
+        cell: ({ row }) => (
+          <div>{row.getValue("frequency")}</div>
+        ),
+    },
+    {
+        accessorKey: "status",
+        header: "Status",
+        cell: ({ row }) => (
+        <div className="capitalize">{row.getValue("status")}</div>
+        ),
+    },
+    {
+        accessorKey: "lastExecutionDate",
+        header: "Last execution date",
+        cell: ({ row }) => (
+            <div>{row.getValue("lastExecutionDate")}</div>
+        ),
+    },
+    {
+        accessorKey: "nextExecutionDate",
+        header: "Next execution date",
+        cell: ({ row }) => (
+            <div>{row.getValue("nextExecutionDate")}</div>
+        ),
+    },
+    {
+        id: "actions",
+        enableHiding: false,
+        cell: ({ row }) => {
+            const schedule = row.original
+
+            return (
+                <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                    <Button variant="ghost" className="h-8 w-8 p-0">
+                        <span className="sr-only">Open menu</span>
+                        <MoreHorizontal />
+                    </Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent align="end">
+                    <DropdownMenuLabel>Actions</DropdownMenuLabel>
+                    <DropdownMenuItem onClick={() => redirect(`/schedules/${schedule.id}`)}>View details</DropdownMenuItem>
+                </DropdownMenuContent>
+                </DropdownMenu>
+            )
+        },
+    },
+]
+
 export function ScheduleTable({ schedules }: ScheduleTableProps) {
-    return (
-        <table>
-            <thead>
-                <tr>
-                    <th>Id</th>
-                    <th>Job slug</th>
-                    <th>Description</th>
-                    <th>Frequency</th>
-                    <th>Status</th>
-                    <th>Last execution date</th>
-                    <th>Next execution date</th>
-                </tr>
-            </thead>
-            <tbody>
-                { schedules.map(schedule => <ScheduleRow key={schedule.id} schedule={schedule} /> ) }    
-            </tbody>
-        </table>
-    )
+  const table = useReactTable({
+    data: schedules,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+  })
+
+  return (
+    <div className="w-full">
+      <div className="rounded-md border">
+        <Table>
+          <TableHeader>
+            {table.getHeaderGroups().map((headerGroup) => (
+              <TableRow key={headerGroup.id}>
+                {headerGroup.headers.map((header) => {
+                  return (
+                    <TableHead key={header.id}>
+                      {header.isPlaceholder
+                        ? null
+                        : flexRender(
+                            header.column.columnDef.header,
+                            header.getContext()
+                          )}
+                    </TableHead>
+                  )
+                })}
+              </TableRow>
+            ))}
+          </TableHeader>
+          <TableBody>
+            {table.getRowModel().rows?.length ? (
+              table.getRowModel().rows.map((row) => (
+                <TableRow
+                  key={row.id}
+                  data-state={row.getIsSelected() && "selected"}
+                >
+                  {row.getVisibleCells().map((cell) => (
+                    <TableCell key={cell.id}>
+                      {flexRender(
+                        cell.column.columnDef.cell,
+                        cell.getContext()
+                      )}
+                    </TableCell>
+                  ))}
+                </TableRow>
+              ))
+            ) : (
+              <TableRow>
+                <TableCell
+                  colSpan={columns.length}
+                  className="h-24 text-center"
+                >
+                  No results.
+                </TableCell>
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
+      </div>
+      <div className="flex items-center justify-end space-x-2 py-4">
+        <div className="space-x-2">
+          <Button
+            variant="outline"
+            size="sm"
+            onClick={() => table.previousPage()}
+            disabled={!table.getCanPreviousPage()}
+          >
+            Previous
+          </Button>
+          <Button
+            variant="outline"
+            size="sm"
+            onClick={() => table.nextPage()}
+            disabled={!table.getCanNextPage()}
+          >
+            Next
+          </Button>
+        </div>
+      </div>
+    </div>
+  )
 }
