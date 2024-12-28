ebe80d9d3f91fd2b07bf270fa75b234738d713c1
fonter <serfin009@gmail.com>
2024-12-28 22:54:55 +0100

    mocked paginaton

--- src/app/schedules/page.tsx
+++ src/app/schedules/page.tsx
@@ -2,9 +2,11 @@ import { Schedule } from "./Schedule"
 import { ScheduleTable } from "./ScheduleTable"
 
 export default async function SchedulesPage() {
+    const initialPage = 1;
+    const pageSize = 100; // TODO: Handle server-side pagination
     const schedules: Schedule[] = []
 
-    await fetch(`${process.env.baseAddress}/api/v1/schedules`)
+    await fetch(`${process.env.baseAddress}/api/v1/schedules?page=${initialPage}&pageSize=${pageSize}`)
         .then(res => res.json())
         .then((json) => schedules.push(...json))
 
